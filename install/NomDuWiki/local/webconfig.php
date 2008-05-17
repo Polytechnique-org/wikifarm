4f5be42f58f097881fccf9c11a987bda8e3860be
Damien Bobillot <damien.bobillot+gitxorg@m4x.org>
2008-05-17 12:17:31 +0200

    Creation d'un webconfig.php par defaut avec les droits necessaires pour etre modifier par apache

---
+++ install/NomDuWiki/local/webconfig.php
@@ -0,0 +1,8 @@
+<?php 
+ $WikiTitle = 'Titre';
+ $Skin = 'gemini';
+ $PageLogoUrl = 'http://www.polytechnique.org/images/skins/default_headlogo.jpg';
+ $DefaultPasswords['read'] = 'public';
+ $DefaultPasswords['edit'] = 'admin';
+?>
+
