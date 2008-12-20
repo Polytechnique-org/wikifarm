f2c94d0fdd1a0eebd6e0daf28b586224d2840f3a
Damien Bobillot <damien.bobillot+gitxorg@m4x.org>
2008-12-20 14:36:35 +0100

    Augmentation de la limite d'upload à 10M

--- install/NomDuWiki/local/config.php
+++ install/NomDuWiki/local/config.php
@@ -1,5 +1,6 @@
 <?php
  $ScriptUrl = 'http://wikifarm.m4x.org/~NomDuWiki';
  $EnableUpload = 1;
+ $UploadMaxSize = 10*1024*1024;
  $XnetWikiGroup = ''; // si vide : wiki administré par les admins x.org
 ?>
