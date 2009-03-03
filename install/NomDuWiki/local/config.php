8a737cbd5e96bc5c8f994ab23bb85ad1bcbef66c
Florent Bruneau <florent.bruneau@polytechnique.org>
2009-03-03 18:50:15 +0100

    Add pmgallery configuration stuff.
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- install/NomDuWiki/local/config.php
+++ install/NomDuWiki/local/config.php
@@ -3,4 +3,8 @@
  $EnableUpload = 1;
  $UploadMaxSize = 10*1024*1024;
  $XnetWikiGroup = ''; // si vide : wiki administré par les admins x.org
+
+ // To use pmgallery
+ $pmGallery['virtualgroups'] = array();
+ include_once("$FarmD/cookbook/pmgallery/pmgallery.php");
 ?>
